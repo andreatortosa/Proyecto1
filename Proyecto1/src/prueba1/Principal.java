9c7bbb19df9a6aa088616d7a09eb86afc7032e92
Andrea <12345678@email.com>
2022-01-24 08:53:45 +0100

    Cambio en fichero Principal

--- Proyecto1/src/prueba1/Principal.java
+++ Proyecto1/src/prueba1/Principal.java
@@ -5,5 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo java-git");
 		System.out.println("Adios");
+		System.out.println("Clase Principal");
+		System.out.println("Pruebas Git. Clase Principal");
 		}
 }
