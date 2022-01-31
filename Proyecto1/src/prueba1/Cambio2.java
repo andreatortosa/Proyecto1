f162adc2368a0c6f6f60d6135a30bcf0e3228bac
Andrea <12345678@email.com>
2022-01-31 08:29:03 +0100

    Validamos Cambio2 en proyecto2

---
+++ Proyecto1/src/prueba1/Cambio2.java
@@ -0,0 +1,10 @@
+package prueba1;
+
+public class Cambio2 {
+	public static void main(String[] args) {
+		System.out.println("Clase Cambio2 en proyecto2");
+		System.out.println("Clase Cambio2 en proyecto2");
+
+	}
+
+}
